30096a7086711d9b92054e596af2f044d2039edd
https://github.com/4darsh23 <adarshrajdas23@gmail.com>
2025-07-11 22:57:48 +0530

    frontend

--- app/page.jsx
+++ app/page.jsx
@@ -11,7 +11,7 @@ export default function Home() {
       <section className="relative overflow-hidden py-32">
         <div className="container mx-auto px-4">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
-            <div className="space-y-8">
+            <div className="space-y-9">
               <Badge
                 variant="outline"
                 className="bg-emerald-900/30 border-emerald-700/30 px-4 py-2 text-emerald-300 text-sm font-medium"
