38f8f6ce5a39e673fb62287961a13040a50549d5
https://github.com/4darsh23 <adarshrajdas23@gmail.com>
2025-07-25 00:42:02 +0530

    adding database

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
-import { ArrowRight, Link, Stethoscope } from "lucide-react";
-import { image } from "next/image";
-import { features } from "@/lib/data";
+import { ArrowRight, Check, Link, Stethoscope } from "lucide-react";
+import { creditBenefits, features, testimonials } from "@/lib/data";
 import { Card, CardAction, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
@@ -100,6 +99,7 @@ export default function Home() {
 
           <div>
             {/*pricing table*/}
+
             <Card className="mt-12 bg-muted/20 border-emerald-900/30">
               <CardHeader>
                 <CardTitle className="text-xl font-semibold text-white flex items-center">
@@ -107,13 +107,101 @@ export default function Home() {
                   Credit System
                 </CardTitle>
 
-                <CardAction>Card Action</CardAction>
+                <CardAction>
+                  <ul className="space-y-3">
+                    {" "}
+                    {creditBenefits.map((benefit, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="flex items-start"
+                        >
+                          <div className="mr-3 mt-1 bg-emerald-900/20 p-1 rounded-full">
+                            <Check className="h-4 w-4 text-emerald-400" />
+                          </div>
+                          <p
+                            className="text-muted-foreground"
+                            dangerouslySetInnerHTML={{ __html: benefit }}
+                          />
+                        </li>
+                      );
+                    })}{" "}
+                  </ul>
+                </CardAction>
               </CardHeader>
-              <CardContent>
-                <p>Card Content</p>
-              </CardContent>
             </Card>
           </div>
+        </div>
+      </section>
+
+      <section className=" py-20 bg-muted/30">
+        <div className="container mx-auto px-4">
+          <div className="text-center mb-16">
+            <Badge
+              variant="outline"
+              className="bg-emerald-900/30 border-b-emerald-700/30 px-4 py-1 text-emerald-400 text-sm font-medium mb-4 "
+            >
+              {" "}
+              Success Stories{" "}
+            </Badge>
+            <h2 className="text-3xl md:text-4xl font-bold text-white mb-4">
+              Users Feedbacks
+              <p className="text-muted-foreground text-lg max-w-2xl mx-auto"> Hear from out Patients and Doctors </p>
+            </h2>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {testimonials.map((testimonials, index) => {
+              return (
+                <Card
+                  key={index}
+                  className="border-emerald-900/20 hover:border-emerald-900/40 transition-all duration-300"
+                >
+                  <CardContent className="pt-6">
+                    <div className=" flex items-center mb-4">
+                      <div className=" w-12 h-12 rounded-full bg-emerald-900/20  justify-center mr-4  flex items-center">
+                        <span className="text-emerald-400 font-bold">{testimonials.initials}</span>
+                      </div>
+                      <div>
+                        <h4 className="font-semibold text-white">{testimonials.name}</h4>
+                        <p className="text-sm text-muted-foreground">{testimonials.role}</p>
+                      </div>
+                    </div>
+                    <p className="text-muted-foreground    ">&quot;{testimonials.quote}&quote</p>
+                  </CardContent>
+                </Card>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+
+      <section className=" py-30">
+        <div className="container mx-auto px-4">
+          <Card className="bg-gradient-to-r from-emerald-900/30 to-emerald-950/20 border-emerald-800/20">
+            <CardContent className="p-8 md:p-12 lg:p-16 relative overflow-hidden">
+              <div>
+                <h2 className="text-3xl md:text-4xl font-bold text-white mb-6">ready to take care of the health</h2>
+              </div>
+              <p className="text-lg text-muted-foreground mb-8">join thousands of users who have simplified their healthcare jounrney with out platform. Get started today and experience healthcare the way it should be</p>
+              <div className="flex flex-col sm:flex-row gap-5">
+                <Button
+                  size="lg"
+                  className="bg-emerald-600 text-white hover:bg-emerald-700"
+                  asChild
+                >
+                  <Link href="/sign-up"> Sign up now </Link>
+                </Button>
+                <Button
+                  asChild
+                  variant="outline"
+                  size="lg"
+                  className="border-emerald-700/30 hover:bg-muted/80"
+                >
+                  <Link href="/pricing"> View Pricing </Link>
+                </Button>
+              </div>
+            </CardContent>
+          </Card>
         </div>
       </section>
     </div>
